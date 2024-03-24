781a116a53a916b0e0289cd95756383c08087041
Vishal-Sahu1 <154455568+Vishal-Sahu1@users.noreply.github.com>
2024-03-24 10:12:42 +0000

    container config done

---
+++ basics/string.js
@@ -0,0 +1,13 @@
+// let fn ='vishal ';
+// let sn = "sahu";
+// console.log(fn + sn + " from" + ' indore');
+// console.log(` hii my name is ${fn} indore ${sn}`);
+// this is a good way of writin 
+const game = new String('deadpiret');
+console.log(game.toUpperCase());
+console.log(game.charAt(2));
+console.log(game.indexOf('d'));
+console.log(game.slice(1,2));
+
+const news = game.substring(0,4);
+console.log(news);
